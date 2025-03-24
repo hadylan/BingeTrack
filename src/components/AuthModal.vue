9227d6c52007aa42ec880d81ca60e6428ad3512f
Dylan Hanique <dylan.han82@gmail.com>
2025-03-24 17:37:21 +0100

    refactor: use uiStore and add login function link

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,7 +1,9 @@
 <script setup>
 import tmdbLogo from '@/assets/tmdb_logo.svg'
-import { useAuthStore } from '@/stores/authStore'
+import { useUIStore } from '@/stores/useUIStore'
+import { useAuthStore } from '@/stores/useAuthStore'
 
+const uiStore = useUIStore()
 const authStore = useAuthStore()
 </script>
 
@@ -12,7 +14,7 @@ const authStore = useAuthStore()
     tabindex="-1"
     aria-hidden="true"
     class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    @click.self="authStore.toggleAuthModal"
+    @click.self="uiStore.closeAuthModal()"
   >
     <div class="relative p-4 w-full max-w-2xl max-h-full">
       <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
@@ -21,7 +23,7 @@ const authStore = useAuthStore()
         >
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Identifiez-vous</h3>
           <button
-            @click="authStore.toggleAuthModal"
+            @click="uiStore.closeAuthModal()"
             type="button"
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
           >
@@ -44,7 +46,7 @@ const authStore = useAuthStore()
           </button>
         </div>
         <div class="p-4 md:p-5 space-y-4">
-          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400 mb-8">
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400 mb-5">
             Pour vous connecter à notre application, vous devez utiliser le système
             d'authentification de TMDB (The Movie Database). <br /><br />
             Ce processus très rapide vous permet d'accéder à vos listes de films et préférences en
@@ -52,10 +54,16 @@ const authStore = useAuthStore()
             l'accès, puis vous reviendrez automatiquement sur notre plateforme. 🚀
           </p>
 
+          <span class="text-orange-400 block"
+            >Pensez à fermer la fenêtre TMDB après l'autorisation pour permettre
+            l'authentification.</span
+          >
+
           <button
             id="tmdb_auth_button"
             type="button"
-            class="bg-gray-800 border-gray-700 text-white hover:bg-gray-900 border font-medium rounded-lg text-sm px-5 mx-auto py-2.5 text-center inline-flex items-center cursor-pointer"
+            class="bg-gray-800 border-gray-700 text-white hover:bg-gray-900 border font-medium rounded-lg text-sm px-5 mx-auto py-2.5 mt-3 text-center inline-flex items-center cursor-pointer"
+            @click="authStore.TMDBLogin()"
           >
             <img :src="tmdbLogo" alt="the movie database logo" class="mr-4" id="tmdb_logo" />
             S'identifier avec TMDB
