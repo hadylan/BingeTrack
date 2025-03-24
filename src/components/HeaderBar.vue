33ccd0b5814be5698e38e02f3772409a01bc6222
Dylan Hanique <dylan.han82@gmail.com>
2025-03-24 17:43:49 +0100

    feat: use UIStore for UI state and add router links for home and profile

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -2,14 +2,19 @@
 import { useMediaQuery } from '@vueuse/core'
 import BurgerMenu from './BurgerMenu.vue'
 import AuthModal from './AuthModal.vue'
-import { useAuthStore } from '@/stores/authStore'
+import { useUIStore } from '@/stores/useUIStore'
+import { useUserStore } from '@/stores/useUserStore'
+import { storeToRefs } from 'pinia'
 
-const authStore = useAuthStore()
+const uiStore = useUIStore()
+const userStore = useUserStore()
 const isMobile = useMediaQuery('(max-width: 768px)')
+
+const { user } = storeToRefs(userStore)
 </script>
 
 <template>
-  <header class="md:px-4">
+  <header class="md:px-4 mb-7">
     <nav class="w-full h-full">
       <div class="flex flex-wrap justify-between">
         <div class="left_section flex items-center" v-if="!isMobile">
@@ -30,23 +35,40 @@ const isMobile = useMediaQuery('(max-width: 768px)')
             </svg>
             <span class="sr-only">Search icon</span>
           </button>
-          <RouterLink to="#" class="ml-7">Films</RouterLink>
+          <RouterLink to="/" class="ml-7">Accueil</RouterLink>
+          <RouterLink to="/movies" class="ml-7">Films</RouterLink>
         </div>
         <h1 class="text-2xl text-white font-semibold">
           <RouterLink to="/">BingeTrack.</RouterLink>
         </h1>
         <div class="right_section flex items-center" v-if="!isMobile">
           <button
+            v-if="user.id === null"
             type="button"
             class="text-black font-medium rounded-lg text-sm px-5 py-2.5 bg-gray-200 hover:bg-gray-300 cursor-pointer"
-            @click="authStore.toggleAuthModal"
+            @click="uiStore.openAuthModal()"
           >
             S'identifier
           </button>
+          <RouterLink v-else to="/profile"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle></svg
+          ></RouterLink>
         </div>
         <BurgerMenu v-if="isMobile" />
       </div>
-      <AuthModal v-if="authStore.isAuthModalOpen" />
+      <AuthModal v-if="uiStore.isAuthModalOpen" />
     </nav>
   </header>
 </template>
