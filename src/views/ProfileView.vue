56f88985c5d37fa0d778a4ddc49d18d0ff63d648
Dylan Hanique <dylan.han82@gmail.com>
2025-03-24 17:47:20 +0100

    feat: add ProfileView to display user movie lists and account management

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useUserStore } from '@/stores/useUserStore'
+import { useAuthStore } from '@/stores/useAuthStore'
+import { storeToRefs } from 'pinia'
+import UserMoviesList from '@/components/UserMoviesList.vue'
+
+const authStore = useAuthStore()
+const userStore = useUserStore()
+const { user } = storeToRefs(userStore)
+</script>
+
+<template>
+  <main>
+    <div id="user_infos_container" class="rounded-xl p-5 mb-5 size-fit">
+      <h2>
+        Votre profil : <span class="font-semibold">{{ user.username }}</span>
+      </h2>
+      <button
+        type="button"
+        class="text-sm text-gray-400 bg-transparent cursor-pointer hover:text-gray-500"
+        @click="authStore.logout()"
+      >
+        Se déconnecter
+      </button>
+    </div>
+
+    <div class="moviesLists">
+      <UserMoviesList
+        v-for="moviesList in user.moviesLists"
+        :key="moviesList.id"
+        :movies="moviesList.movies"
+        :listTitle="moviesList.title"
+        class="mb-5"
+      />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+#user_infos_container {
+  background-color: #202020;
+}
+</style>
