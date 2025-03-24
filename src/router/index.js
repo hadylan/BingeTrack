2e78aaf0b0c08252fce61db12a21588b1402f8f1
Dylan Hanique <dylan.han82@gmail.com>
2025-03-24 17:44:49 +0100

    feat: add movies and profile view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import { useUserStore } from '@/stores/useUserStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,22 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/movies',
+      name: 'movies',
+      component: HomeView,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
+      beforeEnter: (to, from) => {
+        const userStore = useUserStore()
+        if (!userStore.user.id) {
+          return { name: from.name }
+        }
+      },
     },
   ],
 })
