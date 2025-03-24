2dee08534b97b28f464a59b21f62f46938a37933
Dylan Hanique <dylan.han82@gmail.com>
2025-03-24 17:48:16 +0100

    feat: add onMounted hook to check user session and retrieve movies

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderBar from './components/HeaderBar.vue'
+import { onMounted } from 'vue'
+import { useAuthStore } from './stores/useAuthStore'
+import { useUserStore } from './stores/useUserStore'
+
+onMounted(async () => {
+  const authStore = useAuthStore()
+  const userStore = useUserStore()
+
+  await authStore.checkUserSession()
+  await userStore.retrieveMoviesLists()
+})
 </script>
 
 <template>
