897287f43a4fac9a06d7d5fb722768662a14d941
Dylan Hanique <dylan.han82@gmail.com>
2025-04-09 11:02:12 +0200

    refactor: change isMovieListValid first movie object check to id check, add french language params to fetchUserMovieListPage request feat: create rateMovie, toggleMovieInWatchlist and toggleMovieInFavorites functions

--- src/stores/useUserStore.js
+++ src/stores/useUserStore.js
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import apiClient from '@/services/apiClient'
+import { useMovieModalStore } from './useMovieModalStore'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
@@ -45,14 +46,14 @@ export const useUserStore = defineStore('user', {
 
       return (
         freshFirstPage.total_results === cachedList.movies.length &&
-        freshFirstPage.results[0] === cachedList.movies[0]
+        freshFirstPage.results[0].id === cachedList.movies[0].id
       )
     },
 
     async fetchUserMovieListPage(listName, page) {
       try {
         const res = await apiClient.get(`/account/${this.user.id}/${listName}/movies`, {
-          params: { page: page, sort_by: 'created_at.desc' },
+          params: { page: page, sort_by: 'created_at.desc', language: 'fr-FR' },
         })
 
         return res.data
@@ -84,6 +85,115 @@ export const useUserStore = defineStore('user', {
       this.user.movieLists[listName].lastUpdate = Date.now()
       this.user.movieLists[listName].movies = allMovies
       localStorage.setItem(`${listName}`, JSON.stringify(this.user.movieLists[listName]))
+    },
+
+    async rateMovie(rating) {
+      const movieModalStore = useMovieModalStore()
+      const movie = movieModalStore.movie
+
+      try {
+        await apiClient.post(`/movie/${movie.id}/rating?session_id=${this.user.session_id}`, {
+          value: rating * 2,
+        })
+
+        const watchlistMovieIndex = this.user.movieLists.watchlist.movies.findIndex(
+          (mov) => mov.id === movie.id,
+        )
+        if (watchlistMovieIndex >= 0) {
+          this.user.movieLists.watchlist.movies.splice(watchlistMovieIndex, 1)
+          localStorage.setItem('watchlist', JSON.stringify(this.user.movieLists.watchlist))
+        }
+
+        const ratedMovieIndex = this.user.movieLists.rated.movies.findIndex(
+          (mov) => mov.id === movie.id,
+        )
+        if (ratedMovieIndex >= 0) {
+          this.user.movieLists.rated.movies[ratedMovieIndex].rating = rating * 2
+        } else {
+          this.user.movieLists.rated.movies.unshift({
+            id: movie.id,
+            poster_path: movie.poster_path,
+            title: movie.title,
+            rating: rating * 2,
+          })
+        }
+
+        localStorage.setItem('rated', JSON.stringify(this.user.movieLists.rated))
+
+        const favoriteMovieIndex = this.user.movieLists.favorite.movies.findIndex(
+          (mov) => mov.id === movie.id,
+        )
+        if (favoriteMovieIndex >= 0) {
+          this.user.movieLists.favorite.movies[favoriteMovieIndex].rating = rating * 2
+          localStorage.setItem('favorite', JSON.stringify(this.user.movieLists.favorite))
+        }
+      } catch (error) {
+        console.error('Erreur lors de la notation du film', error)
+      }
+    },
+
+    async toggleMovieInWatchlist() {
+      try {
+        const movieModalStore = useMovieModalStore()
+        const movie = movieModalStore.movie
+
+        await apiClient.post(`account/account_id/watchlist?session_id=${this.user.session_id}`, {
+          media_type: 'movie',
+          media_id: movie.id,
+          watchlist: !movieModalStore.movie.isInWatchlist,
+        })
+
+        if (movieModalStore.movie.isInWatchlist === false) {
+          this.user.movieLists.watchlist.movies.unshift({
+            id: movie.id,
+            poster_path: movie.poster_path,
+            title: movie.title,
+            rating: movie.rating,
+          })
+        } else {
+          const watchlistMovieIndex = this.user.movieLists.watchlist.movies.findIndex(
+            (mov) => mov.id === movie.id,
+          )
+          this.user.movieLists.watchlist.movies.splice(watchlistMovieIndex, 1)
+        }
+
+        movieModalStore.movie.isInWatchlist = !movieModalStore.movie.isInWatchlist
+        localStorage.setItem('watchlist', JSON.stringify(this.user.movieLists.watchlist))
+      } catch (error) {
+        console.error("Erreur lors de l'ajout / de la suppression du film dans la watchlist", error)
+      }
+    },
+
+    async toggleMovieInFavorites() {
+      try {
+        const movieModalStore = useMovieModalStore()
+        const movie = movieModalStore.movie
+
+        await apiClient.post(`account/account_id/favorite?session_id=${this.user.session_id}`, {
+          media_type: 'movie',
+          media_id: movie.id,
+          favorite: !movieModalStore.movie.isFavorite,
+        })
+
+        if (movieModalStore.movie.isFavorite === false) {
+          this.user.movieLists.favorite.movies.unshift({
+            id: movie.id,
+            poster_path: movie.poster_path,
+            title: movie.title,
+            rating: movie.rating,
+          })
+        } else {
+          const watchlistMovieIndex = this.user.movieLists.favorite.movies.findIndex(
+            (mov) => mov.id === movie.id,
+          )
+          this.user.movieLists.favorite.movies.splice(watchlistMovieIndex, 1)
+        }
+
+        movieModalStore.movie.isFavorite = !movieModalStore.movie.isFavorite
+        localStorage.setItem('favorite', JSON.stringify(this.user.movieLists.favorite))
+      } catch (error) {
+        console.error("Erreur lors de l'ajout / la suppression du film des favoris", error)
+      }
     },
   },
 })
