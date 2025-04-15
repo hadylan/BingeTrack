ac18a63beca096ab826d375be0d00b63157702b5
Dylan Hanique <dylan.han82@gmail.com>
2025-04-15 16:38:00 +0200

    feat: implement movie search functionality

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,14 +1,38 @@
 <script setup>
+import { ref } from 'vue'
 import { useMediaQuery } from '@vueuse/core'
 import BurgerMenu from './BurgerMenu.vue'
 import AuthModal from './AuthModal.vue'
 import { useUIStore } from '@/stores/useUIStore'
 import { useUserStore } from '@/stores/useUserStore'
 import { storeToRefs } from 'pinia'
+import { useMoviesStore } from '@/stores/useMoviesStore'
+import { useMovieModalStore } from '@/stores/useMovieModalStore'
 
 const uiStore = useUIStore()
 const userStore = useUserStore()
+const movieStore = useMoviesStore()
+const movieModal = useMovieModalStore()
 const isMobile = useMediaQuery('(max-width: 768px)')
+
+const searchResult = ref()
+const showSearchbar = ref(false)
+const searchBarValue = ref()
+let timeoutId
+
+function searchMovie() {
+  clearTimeout(timeoutId)
+
+  timeoutId = setTimeout(async () => {
+    searchResult.value = await movieStore.searchMovieByName(searchBarValue.value)
+  }, 1000)
+}
+
+function closeSearch() {
+  showSearchbar.value = false
+  searchBarValue.value = ''
+  searchResult.value = null
+}
 
 const { user } = storeToRefs(userStore)
 </script>
@@ -18,7 +42,12 @@ const { user } = storeToRefs(userStore)
     <nav class="w-full h-full">
       <div class="flex flex-wrap justify-between">
         <div class="left_section flex items-center" v-if="!isMobile">
-          <button type="button" class="cursor-pointer">
+          <button
+            v-if="!showSearchbar"
+            type="button"
+            class="cursor-pointer"
+            @click="showSearchbar = true"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -35,6 +64,74 @@ const { user } = storeToRefs(userStore)
             </svg>
             <span class="sr-only">Search icon</span>
           </button>
+          <form v-else @submit.prevent="">
+            <label for="search" class="sr-only">Search</label>
+            <div class="relative">
+              <button
+                type="button"
+                class="absolute inset-y-0 start-0 flex items-center ms-3 cursor-pointer"
+              >
+                <svg
+                  class="w-4 h-4 text-gray-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="#141414"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </button>
+              <input
+                v-model="searchBarValue"
+                @input="searchMovie()"
+                id="search"
+                class="block w-full p-2 ps-10 text-sm rounded-lg bg-gray-300 outline-none text-gray-800"
+                placeholder="Search"
+                autocomplete="off"
+                required
+              />
+              <button
+                type="button"
+                class="absolute end-2 bottom-2 cursor-pointer"
+                @click="closeSearch()"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#2d2d2d"
+                  stroke-width="1"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18"></line>
+                  <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </button>
+            </div>
+          </form>
+
+          <ul
+            v-if="searchResult && searchResult.length >= 1"
+            class="absolute top-15 border p-2 rounded-lg bg-gray-300 text-gray-600 font-medium max-h-1/3 overflow-y-scroll"
+          >
+            <li
+              v-for="movie in searchResult"
+              :key="movie.id"
+              @click="(movieModal.openMovieModal(movie.id), closeSearch())"
+              class="px-1 py-2 cursor-pointer hover:text-gray-800"
+            >
+              {{ movie.title }}
+            </li>
+          </ul>
           <RouterLink to="/" class="ml-7">Accueil</RouterLink>
           <RouterLink to="/movies" class="ml-7">Films</RouterLink>
         </div>
