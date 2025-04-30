221ea86f9d7fa323decf2f7051a47fc314008bfd
Dylan Hanique <dylan.han82@gmail.com>
2025-04-30 10:43:04 +0200

    feat: make UserMovieList component responsive

--- src/components/UserMovieList.vue
+++ src/components/UserMovieList.vue
@@ -9,7 +9,7 @@ const props = defineProps(['movieList'])
   <div class="wrapper px-5 pt-5 pb-7 rounded-xl">
     <h2>{{ props.movieList.title }}</h2>
     <div class="movies_list_container overflow-scroll overflow-x-hidden pr-4 mt-4 rounded-xl">
-      <ul class="grid grid-cols-10 gap-3 my-1">
+      <ul class="grid grid-cols-3 grid md:grid-cols-7 lg:grid-cols-10 gap-3 my-1">
         <MovieCard v-for="movie in props.movieList.movies" :key="movie.id" :movie="movie" />
       </ul>
     </div>
